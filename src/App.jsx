098ed8d64e4e2fa85aa95fa29018fd2351839beb
Obebe <shkim000905@naver.com>
2026-04-06 14:25:10 +0900

    feat: colors.css, fonts.css 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import "./styles/colors/colors.css";
+import "./App.css";
+
 function App() {
-  return <div>Part_2 Team3</div>;
+  return (
+    <div className="container">
+      <h1 className="title">Part_2 Team3</h1>
+      <p className="text">CSS Variable Test</p>
+    </div>
+  );
 }
 
 export default App;
