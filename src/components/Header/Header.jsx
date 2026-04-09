34b84c2b011803f10b0799a76329bac5a3e134d5
Obebe <shkim000905@naver.com>
2026-04-09 16:34:18 +0900

    feat:btn_CTA 스터디 만들기 버튼 저장 후 Header에 삽입

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import logo from "../../assets/images/img_logo.svg";
+import btnCTA from "../../assets/images/buttons/btn_CTA.svg";
 import "./header.css";
 
-function Header() {
+function Header({ onCreateStudy }) {
   return (
     <header className="header">
       <div className="inner">
         <div className="header-left">
           <img src={logo} alt="공부의 숲" />
         </div>
-        <div className="header-right">{/* 버튼 컴포넌트 연결 예정 */}</div>
+        <div className="header-right">
+          <button className="btn-cta" onClick={onCreateStudy}>
+            <img src={btnCTA} alt="스터디 만들기" />
+          </button>
+        </div>
       </div>
     </header>
   );
