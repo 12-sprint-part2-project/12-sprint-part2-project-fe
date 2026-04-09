5d3869aae655e51de690f485cf9f4b2be8f5face
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-09 17:34:43 +0900

    feat: Tag 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom";
 import HeaderTest from "./pages/HeaderTest";
+import { Tag } from "./components/Tag/Tag";
 
 import "./App.css";
 
@@ -13,6 +14,15 @@ function App() {
             <div className="container">
               <h1 className="title">Part_2 Team3</h1>
               <p className="text">CSS Variable Test</p>
+
+              <div>
+                <Tag
+                  type="point"
+                  theme="light"
+                  variant="general"
+                  points={100}
+                />
+              </div>
 
               <Link to="/header-test" className="test-btn">
                 Header 테스트 페이지로 이동
---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styles from "./Tag.module.css";
+import pointIcon from "../../assets/images/ic-point.svg";
+
+export const Tag = ({
+  type,
+  theme,
+  variant = "default",
+  points,
+  count,
+  emojiIcon,
+}) => {
+  const config = {
+    point: {
+      icon: <img src={pointIcon} alt="point" />,
+      text: `${points}P 획득`,
+    },
+    emoji: {
+      icon: <img src={emojiIcon} alt="emoji" />,
+      text: count,
+    },
+  };
+
+  return (
+    <div
+      className={`
+        ${styles.tag}
+        ${styles[`tag-${type}`]}
+        ${styles[`tag-${theme}`]}
+        ${variant === "general" ? styles[`tag-${type}-general`] : ""}
+      `}
+    >
+      <div
+        className={`
+          ${styles.content}
+          ${styles[`content-${type}`]}
+        `}
+      >
+        <span
+          className={`
+            ${styles.icon}
+            ${styles[`icon-${type}`]} 
+            ${variant === "general" ? styles[`icon-${type}-general`] : ""}
+          `}
+        >
+          {config[type].icon}
+        </span>
+
+        <span
+          className={`
+            ${styles.text}
+            ${styles[`text-${theme}`]}
+            ${styles[`text-${type}`]}
+            ${variant === "general" ? styles[`text-general`] : ""}
+            ${
+              type === "point" && variant === "general"
+                ? styles[`text-point-general`]
+                : ""
+            }
+          `}
+        >
+          {config[type].text}
+        </span>
+      </div>
+    </div>
+  );
+};
