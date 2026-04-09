532a5ed9a7473173ccf8b34a6d56d151c7ea9cac
Obebe <shkim000905@naver.com>
2026-04-09 18:25:46 +0900

    feat: 반응형 구현, ref: header.css -> Header.mudule.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import logo from "../../assets/images/img_logo.svg";
 import btnCTA from "../../assets/images/buttons/btn_CTA.svg";
-import "./header.css";
+import styles from "./Header.module.css";
 
 function Header({ onCreateStudy }) {
   return (
-    <header className="header">
-      <div className="inner">
-        <div className="header-left">
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <div className={styles.headerLeft}>
           <img src={logo} alt="공부의 숲" />
         </div>
-        <div className="header-right">
-          <button className="btn-cta" onClick={onCreateStudy}>
+        <div className={styles.headerRight}>
+          <button className={styles.btnCta} onClick={onCreateStudy}>
             <img src={btnCTA} alt="스터디 만들기" />
           </button>
         </div>
