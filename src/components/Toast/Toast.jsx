9a79e63be11b45920f989f4056ea26b4c1d17f77
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-04-13 22:14:06 +0900

    chore: Toast UI 주석 설명 추가

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,10 +1,15 @@
 import styles from "./Toast.module.css";
 
-const Toast = ({ type, text, point }) => {
+const Toast = ({ type, text, point = 0 }) => {
+  /**
+   * type: success 또는 warning 2개 타입만
+   * text: 띄울 문구
+   * point: 집중 성공시 쌓일 포인트로 필수값은 아님
+   */
+
   return (
     <div className={`${styles.toast} ${styles[type]}`}>
-      {type === "warning" ? "🚨" : "🎉"} {point > 0 ? point : ""}
-      {text}
+      {type === "warning" ? "🚨" : "🎉"} {(point > 0 ? point : "") + text}
     </div>
   );
 };
