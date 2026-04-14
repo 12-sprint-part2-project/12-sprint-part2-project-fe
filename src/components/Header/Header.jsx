f5c15d096a8c64070edcb322b20ca9a7ffeba624
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-14 09:31:34 +0900

    feat: Header showButton props 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,25 @@ import logo from "../../assets/images/img_logo.svg";
 import Button from "../Button/Button";
 import styles from "./Header.module.css";
 
-function Header({ onCreateStudy }) {
+function Header({ onCreateStudy, showButton = true }) {
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
         <div className={styles.headerLeft}>
           <img src={logo} alt="공부의 숲" />
         </div>
-        <div className={styles.headerRight}>
-          <div className={styles.btnWrapper}>
-            <Button
-              variant="create"
-              label="스터디 만들기"
-              onClick={onCreateStudy}
-              className={styles.ctaText}
-            />
+        {showButton && (
+          <div className={styles.headerRight}>
+            <div className={styles.btnWrapper}>
+              <Button
+                variant="create"
+                label="스터디 만들기"
+                onClick={onCreateStudy}
+                className={styles.ctaText}
+              />
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </header>
   );
--- src/pages/HeaderTest.jsx
+++ src/pages/HeaderTest.jsx
@@ -3,8 +3,13 @@ import Header from "../components/Header/Header";
 function HeaderTest() {
   return (
     <div>
-      <Header />
       <h2>Header 테스트 페이지</h2>
+      <br />
+      <p>버튼 있는 페이지</p>
+      <Header onCreateStudy={() => setModalOpen(true)} />
+      <br />
+      <p>버튼 없는 페이지</p>
+      <Header showButton={false} />
     </div>
   );
 }
