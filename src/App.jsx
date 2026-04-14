8f5a652660f525d638f2371d4d9bebbbf3eeda00
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-14 10:39:38 +0900

    chore: 테스트 페이지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,10 @@
 import { Routes, Route, Link } from "react-router-dom";
-import HeaderTest from "./pages/HeaderTest";
-import TagTest from "./pages/TagTest";
-import CardTest from "./pages/CardTest";
-import ButtonTest from "./pages/ButtonTest";
-import PopupTest from "./pages/PopupTest";
-import ModalTest from "./pages/ModalTest";
-import ToastTest from "./pages/ToastTest";
-import IconTest from "./pages/IconTest";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="container">
-              <h1 className="title">Part_2 Team3</h1>
-              <p className="text">CSS Variable Test</p>
-
-              <Link to="/header-test" className="test-btn">
-                Header 테스트 페이지로 이동
-              </Link>
-              <br />
-              <Link to="/tag-test">Tag 테스트 페이지로 이동</Link>
-              <br />
-              <Link to="/card-test">Card 테스트 페이지로 이동</Link>
-              <br />
-              <Link to="/button-test">Button 테스트 페이지로 이동</Link>
-              <br />
-              <Link to="/popup-test">Popup 테스트 페이지로 이동</Link>
-              <br />
-              <Link to="/modal-test">Modal 테스트 페이지로 이동</Link>
-              <br />
-              <Link to="/toast-test">Toast 테스트 페이지로 이동</Link>
-              <br />
-              <Link to="/icon-test">Icon 테스트 페이지로 이동</Link>
-            </div>
-          }
-        />
-
-        <Route path="/header-test" element={<HeaderTest />} />
-        <Route path="/tag-test" element={<TagTest />} />
-        <Route path="/card-test" element={<CardTest />} />
-        <Route path="/button-test" element={<ButtonTest />} />
-        <Route path="/popup-test" element={<PopupTest />} />
-        <Route path="/modal-test" element={<ModalTest />} />
-        <Route path="/toast-test" element={<ToastTest />} />
-        <Route path="/icon-test" element={<IconTest />} />
-      </Routes>
+      <Routes>{/* 추후 페이지 라우트 추가 예정 */}</Routes>
     </>
   );
 }
