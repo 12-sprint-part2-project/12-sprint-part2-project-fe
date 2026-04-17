f4af00b215f1bd1ccaa9cae077765e03f1bd0da8
narin116 <aasdff1010@gmail.com>
2026-04-17 09:44:10 +0900

    구조: 폴더 구조 생성

---
+++ src/pages/StudyDetailPage/components/PasswordModal/PasswordModal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PasswordModal.module.css";
+
+const PasswordModal = () => {
+  return <div>PasswordModal</div>;
+};
+
+export default PasswordModal;
