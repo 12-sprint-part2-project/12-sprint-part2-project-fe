cec061295204045e15e41f71023cd8fd1298b093
narin116 <aasdff1010@gmail.com>
2026-04-17 18:26:59 +0900

    feat: 삭제 버튼 구현 완료. (아직 비밀번호 세션 검증은 안함)

--- src/pages/StudyDetailPage/StudyDetail.jsx
+++ src/pages/StudyDetailPage/StudyDetail.jsx
@@ -4,8 +4,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import Tag from "../../components/Tag/Tag";
 import BoxHeaderInfo from "../../components/BoxHeader/BoxHeaderInfo";
 import NavButton from "../../components/NavButton/NavButton";
-import { getStudyDetail } from "../../api/studies";
+import { deleteStudy, getStudyDetail } from "../../api/studies";
 import PasswordModal from "./components/PasswordModal/PasswordModal";
+import Popup from "../../components/Popup/Popup";
 
 const StudyDetail = () => {
   const { id = 1 } = useParams();
@@ -27,33 +28,73 @@ const StudyDetail = () => {
 
   /* 공유/삭제/수정 버튼 관련 상태 변수 */
   const [showModal, setShowModal] = useState(false); //비밀번호 모달을 보여주는 상태
-
-  const [formType, setFormType] = useState(""); //생성/수정의 경우, 비밀번호 인증 성공 시 실행할 form의 type. (create, modify)
+  const [showDeletePopup, setShowDeletePopup] = useState(false); //정말 삭제하시겠습니까? 팝업.
   const [confirmText, setConfirmText] = useState(""); //버튼마다 비밀번호 모달 내 버튼명이 다르므로, 여기에 저장해서 사용.
-  //근데 이건 현재 페이지의 UI로 보이는 게 아니라서, 그냥 let 변수로 관리하는 게 더 부하 적을 것이라 판단.
+  const [pwType, setPwType] = useState("");
+
   /* 공유/수정/삭제 버튼 클릭 핸들러 */
   const onClickModify = () => {
+    //먼저 세션 확인!!!!! 세션에 없다면 비밀번호 모달 실행
     //비밀번호 체크 모달 -> 비밀번호 일치할 시 -> 수정 페이지로 Link
-    setFormType("modify");
+    setPwType("modify");
     setConfirmText("수정하러 가기");
     setShowModal(true);
   };
-  const onClickDelete = () => {};
+  const onClickDelete = () => {
+    setPwType("delete");
+    setConfirmText("삭제하기");
+    setShowModal(true);
+  };
   const onClickSharing = () => {};
+
+  //비밀번호 인증 성공 시 실행할 함수.
+  const onPasswordSuccess = () => {
+    switch (pwType) {
+      case "modify":
+        navigate(`/studies/new`, {
+          state: { type: "modify", study },
+        });
+        break;
+      case "delete":
+        //진짜로 삭제할거냔 거 표시하기 -> 취소버튼 누르면, 원래 상세 페이지로. 확인 버튼 누르면 -> delete api 호출
+        setShowDeletePopup(true);
+        break;
+      //TODO: 여기에 집중 진입, 습관 진입 시의 코드도 적을 수 있을 것 같습니다!
+      default:
+        break;
+    }
+  };
+
+  //삭제 버튼 -> 비밀번호 입력 -> 인증 성공 -> 정말 삭제하시겠습니까? 네 -> 하고나서 실행 될 함수.
+  const handleDelete = async () => {
+    try {
+      await deleteStudy(id);
+    } catch (e) {
+      //TODO: 삭제 실패 시 toast 띄우기
+      console.log("삭제 실패 =>", e);
+    }
+    setShowDeletePopup(false);
+  };
 
   return (
     <section className={styles.box}>
-      {showModal && study && (
-        <PasswordModal
-          setShowModal={setShowModal}
-          studyId={id}
-          title={study.title}
-          confirmText={confirmText}
-          onPasswordSuccess={() => {
-            navigate(`/studies/new`, {
-              state: { type: formType, study },
-            });
-          }}
+      {showModal &&
+        study && ( //비밀번호 모달 띄우기
+          <PasswordModal
+            setShowModal={setShowModal}
+            studyId={id}
+            title={study.title}
+            confirmText={confirmText}
+            onPasswordSuccess={onPasswordSuccess}
+          />
+        )}
+      {showDeletePopup && ( //정말 삭제하시겠습니까? 팝업
+        <Popup
+          setShowPopup={setShowDeletePopup}
+          hasCancel={true}
+          message="정말 삭제하시겠습니까?"
+          onClickConfirm={handleDelete}
+          onClickCancel={() => setShowDeletePopup(false)}
         />
       )}
       <div className={styles.boxHeader}>
