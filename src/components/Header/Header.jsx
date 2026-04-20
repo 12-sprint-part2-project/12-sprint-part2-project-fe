18eb101b270a48c14b37b4f1e28365f135e36acc
Obebe <shkim000905@naver.com>
2026-04-20 15:14:37 +0900

    fix: Header 컴포넌트 img onClick을 Link로 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../assets/images/logos/logo.svg";
 import Button from "../Button/Button";
 import styles from "./Header.module.css";
 
 function Header({ onCreateStudy, showButton = true }) {
-  const navigate = useNavigate();
-
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
         <div className={styles.headerLeft}>
-          <img
-            src={logo}
-            alt="공부의 숲"
-            onClick={() => navigate("/")}
-            style={{ cursor: "pointer" }}
-          />
+          <Link to="/">
+            <img src={logo} alt="공부의 숲" />
+          </Link>
         </div>
         {showButton && (
           <div className={styles.headerRight}>
