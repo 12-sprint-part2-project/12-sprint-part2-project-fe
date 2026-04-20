9d37ebe715f662b25da5914fc71b415510d85b11
Obebe <shkim000905@naver.com>
2026-04-20 14:21:11 +0900

    fix: logo/logo.svg 적용, Header 컴포넌트 로고 클릭 시 루트 페이지로 이동

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
-import logo from "../../assets/images/img_logo.svg";
+import { useNavigate } from "react-router-dom";
+import logo from "../../assets/images/logos/logo.svg";
 import Button from "../Button/Button";
 import styles from "./Header.module.css";
 
 function Header({ onCreateStudy, showButton = true }) {
+  const navigate = useNavigate();
+
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
         <div className={styles.headerLeft}>
-          <img src={logo} alt="공부의 숲" />
+          <img
+            src={logo}
+            alt="공부의 숲"
+            onClick={() => navigate("/")}
+            style={{ cursor: "pointer" }}
+          />
         </div>
         {showButton && (
           <div className={styles.headerRight}>
