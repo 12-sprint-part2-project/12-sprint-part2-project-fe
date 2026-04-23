d9131fd5d1e8c8e4c0ad9a128ced96710bb43470
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-23 16:45:08 +0900

    feat: 타이머 세 번째 버튼을 종료/리셋으로 변경

--- src/pages/FocusPage/Focus.jsx
+++ src/pages/FocusPage/Focus.jsx
@@ -147,6 +147,10 @@ function Focus() {
     } catch {}
   };
 
+  const handleStop = () => {
+    setShowStopConfirmPopup(true);
+  };
+
   // 재진입 팝업에서 "계속 진행" 선택 시 일시정지된 타이머를 재시작하고 팝업 닫기
   const handleResumeConfirm = () => {
     resume();
@@ -292,6 +296,7 @@ function Focus() {
             onStart={handleStartClick}
             onPause={pause}
             onResume={resume}
+            onStop={handleStop}
           />
         </div>
       </div>
