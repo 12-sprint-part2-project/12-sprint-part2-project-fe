00deaedc5e8815cc484b07e714b72c961bf73ea5
narin116 <aasdff1010@gmail.com>
2026-04-27 09:33:23 +0900

    refactor: StudyForm 정리 완료

--- src/pages/StudyFormPage/components/Title/Title.jsx
+++ src/pages/StudyFormPage/components/Title/Title.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "./Title.module.css";
 
+/*
+    Study Form 페이지의 타이틀 명
+    type
+    ㄴ create : 스터디 만들기
+    ㄴ modify : 스터디 수정
+  */
+
 const Title = ({ type }) => {
   return (
     <h2 className={styles.formTitle}>
